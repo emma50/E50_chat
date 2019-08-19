8aff2b7bc2f1d1afc0faa84c5dafd7a4d0cd97b7
Your Name <you@example.com>
2019-08-19 17:28:32 +0100

    Add newMessage and createMessage events

---
+++ public/js/index.js
@@ -0,0 +1,30 @@
+// initiating the request. making a request from the client to the server to open up a web socket and keep 
+// the connection open we make use of the below method available to us because we loaded the above library
+const socket = io();    
+        
+socket.on("connect", function () {  // "connect" a built-in socket.io event
+    console.log("Connected to server");
+
+    socket.emit("createMessage", {
+        to: "john@example.com",
+        text: "Hey! This is a new Message"
+    })
+
+    // socket.emit("createEmail", {
+    //     to: "john@example.com",
+    //     text: "Hey! This is Emmanuel",
+    //     createdAt: new Date()
+    // })
+})
+
+socket.on("disconnect", function () {  // "disconnect" a built-in socket.io event 
+    console.log("Disconnected from server");
+})
+
+socket.on("newMessage", function (message) {  // "newMessage" a custom event --- this listens to the newMessage event emitted/created on server.js
+    console.log("new Message:", message);
+})
+
+// socket.on("newEmail", function (email) {  // "newEmail" a custom event --- this listens to the newEmail event emitted/created on server.js
+//     console.log("new Email:", email);
+// })
--- server/server.js
+++ server/server.js
@@ -8,19 +8,40 @@ const port = process.env.PORT || 3000;
 const app = express();
 const server = http.createServer(app);
 
+// express middleware
+app.use(express.static(publicPath));
+
 // configure server to use socket.io
 const io = socketIO(server)       // here is how we communicate between the server and client
 
-// register an event listener with .on() --- "connection" built-in socket.io event
+// register an event listener with .on() method --- "connection" built-in socket.io event
 io.on("connection", (socket) => {  // the socket argument here is similar to the one we had access to in index.html. it represents the individual socket
     console.log("New user connected");
+    
+    // socket.emit("newEmail", {    // this starts up the newEmail event --- the server received an email send it to the client 
+    //     from: "emmanuel@example.com",
+    //     text: "Hey watsup!",
+    //     createdAt: new Date()
+    // });   
 
-    socket.on("disconnect", () => {  
+    socket.emit("newMessage", {    // this starts up the newMessage event --- the server received a message send it to the client 
+        from: "daniella@example.com",
+        text: "Hey!",
+        createdAt: new Date()
+    });    
+
+    socket.on("createMessage", (newMessage) => {    // this is a custom event 
+        console.log("createMessage:", newMessage);
+    })    
+
+    // socket.on("createEmail", (newEmail) => {    // this is a custom event 
+    //     console.log("createEmail:", newEmail);
+    // })  
+
+    socket.on("disconnect", () => {    // this listens to the disconnect event in index.js
         console.log("User disconnected");
     })
 })
-
-app.use(express.static(publicPath));
 
 server.listen(port, () => {
     console.log(`server running on port ${port}`);
